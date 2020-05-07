276337568955a0ff95cd527d3e033ff577830974
cho881020 <kj_cho@idealidea.co.kr>
2020-05-07 20:20:52 +0900

    스피너 어댑터 뼈대 작업 진행중

---
+++ app/src/main/java/kr/tjeit/editmyinfopractice_20200506/adapters/CategorySpinnerAdapter.kt
@@ -0,0 +1,28 @@
+package kr.tjeit.editmyinfopractice_20200506.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kr.tjeit.editmyinfopractice_20200506.R
+import kr.tjeit.editmyinfopractice_20200506.datas.Category
+
+class CategorySpinnerAdapter(val mContext: Context, val resId:Int, val mList:ArrayList<Category> ) : ArrayAdapter<Category>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        tempRow?.let {
+
+        }.let {
+            tempRow = inf.inflate(R.layout.pizza_store_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
+}
